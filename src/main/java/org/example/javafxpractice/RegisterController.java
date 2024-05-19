e69923289d14c5008434c934ac36c27f9e9a091d
Leon <galileon.destura@gmail.com>
2024-05-19 17:42:23 +0800

    Version 1

---
+++ src/main/java/org/example/javafxpractice/RegisterController.java
@@ -0,0 +1,17 @@
+package org.example.javafxpractice;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.PasswordField;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.sql.Connection;
+import java.sql.Statement;
+
+
+public class LoginscreenController {
+
+}

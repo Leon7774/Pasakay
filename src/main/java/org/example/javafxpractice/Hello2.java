5c5f84ad380449939276656b973870bda7041b53
Leon <galileon.destura@gmail.com>
2024-05-19 14:10:32 +0800

    First Practice

--- src/main/java/org/example/javafxpractice/Hello2.java
+++
@@ -1,7 +0,0 @@
-package org.example.javafxpractice;
-
-public class Hello2 {
-    public static void main(String[] args) {
-        System.out.println("Hi, I am a new file!");
-    }
-}
--- src/main/java/org/example/javafxpractice/Hello3.java
+++
@@ -1,2 +0,0 @@
-package org.example.javafxpractice;public class Hello3 {
-}

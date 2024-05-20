790b9e710bf8b5b8d99d7ce81d93d0cda9405f04
Leon <galileon.destura@gmail.com>
2024-05-20 09:01:20 +0800

    Version 1.2

--- src/main/java/org/example/javafxpractice/LoginController.java
+++ src/main/java/org/example/javafxpractice/LoginController.java
@@ -11,8 +11,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 
 import javax.swing.*;
+import javax.swing.plaf.nimbus.State;
 import java.io.File;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 
@@ -23,17 +27,26 @@ public class LoginController implements Initializable {
     @FXML
     public Button loginButton;
     @FXML
+    public Button registerButton;
+    @FXML
     private Label loginLabel;
     @FXML
     private PasswordField loginPasswordField;
     @FXML
     private Button closeButton;
 
-    @FXML
+
     public void onClickLoginButton(ActionEvent event) {
-        loginLabel.setText("You clicked login");
-        loginLabel.setVisible(true);
-        System.out.println("Yes");
+        if (loginUsernameTextField.getText().isBlank()) {
+            loginLabel.setText("Username is empty");
+            loginLabel.setVisible(true);
+        } else if (loginPasswordField.getText().isBlank()) {
+            loginLabel.setText("Password is empty");
+            loginLabel.setVisible(true);
+        } else {
+            isValidLogin();
+            System.out.println("Yes");
+        }
     }
 
     public void closeButtonOnActionEvent(ActionEvent event) {
@@ -48,5 +61,33 @@ public class LoginController implements Initializable {
     }
 
     public void isValidLogin() {
+        DatabaseConnection dbConnection = new DatabaseConnection();
+        Connection connection1 = dbConnection.getConnection();
+
+        String verifyLogin = "SELECT count(1) FROM userlist WHERE userName = '" + loginUsernameTextField.getText() + "'AND password ='" + loginPasswordField.getText() + "'";
+
+        try {
+            Statement statement = connection1.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
+
+            while (queryResult.next()) {
+                if (queryResult.getInt(1) == 1) {
+                    loginLabel.setText("Login Sucessful");
+                    loginLabel.setVisible(true);
+                }else{
+                    loginLabel.setText("No matches for that user/password combination was found.");
+                    loginLabel.setVisible(true);
+                }
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    public void registerClicked(ActionEvent event) {
+
     }
 }
---
+++ src/main/java/org/example/javafxpractice/RegisterController.java
@@ -0,0 +1,12 @@
+package org.example.javafxpractice;
+
+import javafx.scene.control.*;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class RegisterController {
+
+}

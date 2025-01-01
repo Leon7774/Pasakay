94f14f816d6f1c590f115b52a69fcc22d5a837b1
Lester-ozu <jlc628549@gmail.com>
2025-01-02 01:27:45 +0800

    Version 5.9.10

--- src/java/main/objects/RentalTransaction.java
+++ src/java/main/objects/RentalTransaction.java
@@ -5,27 +5,30 @@ import java.util.Objects;
 
 public class RentalTransaction {
 
-    int rentalID;
+    int rentalID, transactionID;
     String transactionName, date;
     double amount;
 
-    public RentalTransaction(int rentalID, String transactionName, double amount, String date) {
+    public RentalTransaction(int transactionID, int rentalID, String transactionName, double amount, String date) {
 
         this.rentalID = rentalID;
         this.transactionName = transactionName;
         this.amount = amount;
         this.date = date;
+        this.transactionID = transactionID;
     }
 
     public void setRentalID(int rentalID) {this.rentalID = rentalID;}
     public void setTransactionName(String transactionName) {this.transactionName = transactionName;}
     public void setAmount(double amount) {this.amount = amount;}
     public void setDate(String date) {this.date = date;}
+    public void setTransactionID(int transactionID) {this.transactionID = transactionID;}
 
     public String getTransactionName() {return this.transactionName;}
     public double getAmount() {return this.amount;}
     public String getDate() {return this.date;}
     public int getRentalID() {return this.rentalID;}
+    public int getTransactionID() {return this.transactionID;}
 
     public int hashCode() {
 
